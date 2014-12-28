baeeb98f91dd6317906e436fcda40f7fc51caa9f
Alexandru Iovan <iovan_alexandru@hotmail.com>
2014-12-28 21:40:25 +0200

    Added different forecast view types.

--- app/src/main/java/com/example/android/sunshine/app/model/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/model/ForecastAdapter.java
@@ -16,13 +16,40 @@ import com.example.android.sunshine.app.ui.main.ForecastFragment;
  * ForecastAdapter
  */
 public class ForecastAdapter extends CursorAdapter {
+    private final int VIEW_TYPE_TODAY = 0;
+    private final int VIEW_TYPE_FUTURE_DAY = 1;
+
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
 
     @Override
+    public int getItemViewType(int position) {
+        return (position == 0) ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
+        // Choose the layout type
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId;
+        switch (viewType) {
+            case VIEW_TYPE_TODAY:
+                layoutId = R.layout.list_item_forecast_today;
+                break;
+            case VIEW_TYPE_FUTURE_DAY:
+                layoutId = R.layout.list_item_forecast;
+                break;
+            default:
+                throw new UnsupportedOperationException("View type " + viewType + " is unknown");
+        }
+
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
     }
 
     @Override
